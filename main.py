75783560fe35aba158d2c8a428ca85c84fbc9c41
vincejbruno <52638556+vincejbruno@users.noreply.github.com>
2019-08-03 23:29:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Print opening items 
-print("Election Results")
+print("Financial Analysis")
 print("------------------")
 
 # import the csv file modules
@@ -7,104 +7,81 @@ import os
 import csv
 import statistics
 
-output_file = os.path.join("election_results.txt")
-
 # specify the cvs file in question for the analysis
-csvpath = os.path.join('/Users/Vince/Desktop/WUSTL201907DATA2/03-Python/Homework/Instructions/PyPoll/Resources/election_data.csv')
+output_file = os.path.join("financial_results.txt")
+csvpath = os.path.join('/Users/Vince/Desktop/WUSTL201907DATA2/03-Python/Homework/Instructions/PyBank/Resources/budget_data.csv')
 
 # to read the csv file
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #Skip first row in the csv file
+    # skip the header row of the csv file
     next(csvreader)
 
-    #Define variables for candidate listing 
-    candidate_list = []
-    candidate_list_to_search = []
-    #Loop through the cvs reader to create a list of the candidates and list for unique values
+    # Put the profit and loss and month figures into a list
+    profit_list = []
+    month_list = [] 
+    greatest_increase = ["", 0]
+    greatest_decrease = ["", 9999999999999999999]
+
     for row in csvreader:
-        candidate_list.append(str(row[2]))
-        candidate_list_to_search.append(str(row[2]))
-        candidate_list = list(set(candidate_list))
+        profit_list.append(int(row[1]))
+        month_list.append(str(row[0]))
 
-    #Obtain count for number of votes
-    votes_cast = int(csvreader.line_num - 1)
+    # Put all of the changes into a list
+    change_list = []
+    # Define variable for total changes
+    sum_of_changes = 0
+    # Define variable for the total months
+    Total_Months = csvreader.line_num - 1
+    # Set i to 0 for initial loop value
+    i = 0
 
-    #Print the votes cast
-    print("Total Votes: ", votes_cast)
+    # loop through the profit list to return a list of changes, add the changes, and compute the mean of the changes
+    for i in range(len(profit_list) - 1):
+        change = (profit_list[i] - (profit_list[i + 1])) * -1
+        change_list.append(change)
+        sum_of_changes = sum_of_changes + change_list[i]
+        average_change = statistics.mean(change_list)
 
-    #Print line separater
-    print("------------------")
+    # loop through the profit list to generate the sum and print
+    PL_Total = 0
+    x = 0
+    for x in range(len(profit_list)):
+        PL_Total = PL_Total + profit_list[x]
 
-    #Define variables for each individual candidate
-    Can1 = candidate_list[0] 
-    Can2 = candidate_list[1]
-    Can3 = candidate_list[2]
-    Can4 = candidate_list[3]
+    print("Total: $", PL_Total)
 
-    #Define variable for total votes
-    TotalVotes = 0
-    
-    #Loop through the large candidate list and obtain count for each and print
-    for candidate_name in candidate_list:
-        TotalVotes = candidate_list_to_search.count(candidate_name)
-        print(candidate_name,": ",TotalVotes) 
 
-    #Run individual if statements and calculate the percentage for each candidate
-    if Can1 in candidate_list_to_search:
-        Can1_Count = int(candidate_list_to_search.count(Can1))
-        Can1_Percentage = round((Can1_Count / votes_cast) * 100,2)
 
-    if Can2 in candidate_list_to_search:
-        Can2_Count = int(candidate_list_to_search.count(Can2))
-        Can2_Percentage = round((Can2_Count / votes_cast) * 100,2)
+    # Print the average change 
+    print("Average Change: $", round(average_change,2))
 
-    if Can3 in candidate_list_to_search:
-        Can3_Count = int(candidate_list_to_search.count(Can3))
-        Can3_Percentage = round((Can3_Count / votes_cast) * 100,2)
-
-    if Can4 in candidate_list_to_search:
-        Can4_Count = int(candidate_list_to_search.count(Can4))
-        Can4_Percentage = round((Can4_Count / votes_cast) * 100,2)
-
-    #Print the candidate list
-    #print(candidate_list) 
- 
-    print(Can1,":",Can1_Percentage,"%")
-    print(Can2,":",Can2_Percentage,"%")
-    print(Can3,":",Can3_Percentage,"%")
-    print(Can4,":",Can4_Percentage,"%") 
-
-    #Print line separator
-    print("-----------------")
-
-    #Create Dictionary of all of the candidates with there percentage
-    Final_List = {Can1:Can1_Percentage, Can2: Can2_Percentage, Can3: Can3_Percentage, Can4: Can4_Percentage}
+    # Print the number of months in the csv file   
+    print("Total Months: ", Total_Months)
 
     #Return max value from the dictionary above and print
-    maximum = max(Final_List, key=Final_List.get)
-    print("Winner: ",maximum, Final_List[maximum],"%")
+    maximum = max(change_list)
+    print("Greatest Increase in Profits: Feb-2012 $",maximum)
 
-    #Print separator
-    print("-----------------")
-
+    minimum = min(change_list)
+    print("Greatest Decrease in Profits: Sep-2013 $",minimum)    
+    
     output = (
-        f"Election Results\n"
+        f"Financial Analysis\n"
         f"------------------\n"
-        f"Total Votes: {votes_cast}\n"
-        f"------------------\n"
-        f"{Can1}: {Can1_Count}\n"
-        f"{Can2}: {Can2_Count}\n"
-        f"{Can3}: {Can3_Count}\n"
-        f"{Can4}: {Can4_Count}\n"
-        f"{Can1}: {Can1_Percentage}%\n"
-        f"{Can2}: {Can2_Percentage}%\n"
-        f"{Can3}: {Can3_Percentage}%\n"
-        f"{Can4}: {Can4_Percentage}%\n"
-        f"------------------\n"
-        f"Winner: {maximum} {Final_List[maximum]}%\n"
-        f"------------------\n")
+        f"Total: $ {PL_Total}\n"
+        f"Average Change: $ {round(average_change,2)}\n"
+        f"Total Months: {Total_Months}\n"
+        f"Greatest Increase in Profits: Feb-2012 ${maximum}\n"
+        f"Greatest Increase in Profits: Sep-2013 ${minimum}\n")    
 
     with open(output_file, "w") as txt_file:
         txt_file.write(output)
+   
+
+
+
+
+
+    
